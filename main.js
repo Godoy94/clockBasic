d29729a9d722be8f0493d530a0aa553434c88eca
Godoy94 <domingues.godoy@hotmail.com.br>
2020-08-04 19:59:01 -0300

    adjusting variables names

--- main.js
+++ main.js
@@ -6,8 +6,11 @@ var interval = setInterval(function clock(){
 
     var writeHours = document.getElementById('horas');
     writeHours.innerHTML = hours;
+
     var writeMinutes = document.getElementById('minutos');
     writeMinutes.innerHTML = minutes;
+
     var writeSeconds = document.getElementById('segundos');
     writeSeconds.innerHTML = seconds;
+    
 }, 100)
